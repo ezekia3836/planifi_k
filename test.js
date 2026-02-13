af7371a875056bff9e80a56bd4c866bfb03abb05
ezekia <ezekia.kontiki@yahoo.com>
2026-02-13 19:07:53 +0300

    report

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-fetch("https://konticreav2.kontikimedia.fr:5009/api/creativities/filter-plannifik?focus_id=384141&base_id=62", {
+fetch("https://konticreav2.kontikimedia.fr:5009/api/creativities/filter-plannifik?focus_id=391529&base_id=118", {
   method: "POST"
 })
 .then(res => res.json())
